c1470117959218858e2da4d8971d5801153e7f4f
José Lafuente <joselafuente77@gmail.com>
2024-12-30 08:28:23 +0100

    Add Geolocaction and map Leaflet

---
+++ Services/GeolocationService .cs
@@ -0,0 +1,21 @@
+﻿//using Microsoft.JSInterop;
+//using System.Threading.Tasks;
+
+//namespace METEOdotNet.Services
+
+//{
+//    public class GeolocationService
+//    {
+//        private readonly IJSRuntime _jsRuntime;
+//        public GeolocationService(IJSRuntime jsRuntime)
+//        {
+//            _jsRuntime = jsRuntime;
+//        }
+
+//        public async Task<(double Latitude, double Longitude)> GetGeolocationAsync()
+//        {
+//            var result = await _jsRuntime.InvokeAsync<dynamic>("getGeolocation");
+//            return ((double)result.latitude, (double)result.longitude);
+//        }
+//    }
+//}
